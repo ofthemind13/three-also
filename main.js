94a4cbe04202060d2468443ab56323cc5b53f79c
ofthemind13 <80801709+ofthemind13@users.noreply.github.com>
2023-06-18 11:59:16 -0600

    experimentation

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+function main() {
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+  const fov = 75;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+  const scene = new THREE.Scene();
 
-camera.position.z = 5;
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-function animate() {
-	requestAnimationFrame( animate );
+  const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
-animate();
-
-
-
+main();
